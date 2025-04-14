a7b2cf6e5cc7c2fb2b9b41e5311d914be443245f
oseiw <ioannuscassianus@gmail.com>
2025-04-14 18:13:18 -0400

    hell yeah

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.example.financetracker","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"com.example.financetracker","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
